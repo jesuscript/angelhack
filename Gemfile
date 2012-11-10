74adb76f0d2892ba568d948edf676d992952e206
Megatv <konstantin.k.ed@gmail.com>
2012-11-10 12:03:31 +0000

    first commit

---
+++ Gemfile
@@ -0,0 +1,21 @@
+source 'https://rubygems.org'
+
+gem 'rails', '3.2.6'
+
+# Bundle edge Rails instead:
+# gem 'rails', :git => 'git://github.com/rails/rails.git'
+
+gem 'mysql2'
+gem 'foreigner'
+
+
+# Gems used only for assets and not required
+# in production environments by default.
+group :assets do
+  gem 'therubyracer', :require => 'v8'
+  gem 'less-rails'
+  gem 'uglifier', '>= 1.0.3'
+end
+
+gem 'jquery-rails'
+
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ANGELHACK::Application.config.secret_token = '1581f07a758313be1a20d8978ffbe96998e40c55e30e7ed7b4f814a8287d694d9d0dd8b841fe242adaf1ae3fa8092f32ac426235096c9f656d6a96abb1824d05'
